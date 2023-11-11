1a548238e0cd7c272ffb059a453465a11c2a9878
Rohit Narnaware <rnarnaware@cogitate.us>
2023-11-12 04:03:49 +0530

    added

---
+++ bin/cli.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+const { execSync } = require('child_process');
+const { inherits } = require('util');
+
+const runCommand = command => {
+    try {
+        execSync(`${command}`, { stdio: 'inherit'});
+    }catch(e){
+        console.log('failed to exec ', e);
+        return false;
+    }
+    return true;
+}
+
+const repoName = process.argv[2];
+const giCheckoutCommand = `git clone --depth 1 https://git.cogitate.us/rnarnaware/demo ${repoName}`
+const installDepsCommand = `cd ${repoName} && npm install`;
+
+console.log(`cloning the repo with name ${repoName}`);
+const checkOut = runCommand(giCheckoutCommand);
+
+if(!checkOut) process.exit(-1);
+
+console.log('insallling dependnecies for ', repoName);
+const installedlDeps = runCommand(installDepsCommand)
+if(!installedlDeps) process.exit(-1);
+console.log(`congrats happy hacking`);
