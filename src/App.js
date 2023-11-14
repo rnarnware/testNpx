3c15fa0aac237f40bb91d2f0137b31f3a8b9e5c1
Rohit Narnaware <rnarnaware@cogitate.us>
2023-11-14 11:54:08 +0530

    added header and navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './headers/header';
 
 function App() {
   return (
     <div className="App">
-   <h1>Hi rohit</h1>
+      <Header />
+     <h1>Welcome</h1>
     </div>
   );
 }
