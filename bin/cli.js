7f1e0bf97015cded8d928a7584f72d195816e758
Rohit Narnaware <rnarnaware@cogitate.us>
2023-11-14 12:57:48 +0530

    upadted code

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const { execSync } = require('child_process');
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 
 const runCommand = command => {
