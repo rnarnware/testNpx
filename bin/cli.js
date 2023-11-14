8637e2800b9bf009705e7e60ea3b7869629848bd
Rohit Narnaware <rnarnaware@cogitate.us>
2023-11-14 14:00:19 +0530

    trying

--- bin/cli.js
+++ bin/cli.js
@@ -14,8 +14,16 @@ const runCommand = command => {
 }
 
 const repoName = process.argv[2];
-const filesToCopy = process.argv[3] ? process.argv[3].split(',') : []; // User can provide comma-separated folder names
-const gitCheckoutCommand = `git clone --depth 1 https://github.com/rnarnware/testNpx ${repoName}`;
+const folderName = process.argv[3];
+
+if (!repoName || !folderName) {
+    console.log('Usage: npx create-react-casper <repoName> <folderName>');
+    process.exit(-1);
+}
+
+const gitRepoURL = 'https://github.com/rnarnware/testNpx';
+const gitCheckoutCommand = `git clone --depth 1 --filter=blob:none --no-checkout ${gitRepoURL} ${repoName}`;
+const gitSparseCheckoutCommand = `cd ${repoName} && git sparse-checkout set ${folderName}`;
 const installDepsCommand = `cd ${repoName} && npm install`;
 
 console.log(`Cloning the repo with name ${repoName}`);
@@ -26,43 +34,19 @@ if (!checkout) {
     process.exit(-1);
 }
 
+console.log(`Checking out the specific folder: ${folderName}`);
+const sparseCheckout = runCommand(gitSparseCheckoutCommand);
+
+if (!sparseCheckout) {
+    console.log(`Failed to checkout the specific folder: ${folderName}`);
+    process.exit(-1);
+}
+
 console.log('Installing dependencies for ', repoName);
 const installedDeps = runCommand(installDepsCommand);
 if (!installedDeps) {
     console.log('Failed to install dependencies');
     process.exit(-1);
-}
-
-if (filesToCopy.length > 0) {
-    console.log('Copying specific files/folders');
-    filesToCopy.forEach(folder => {
-        const sourcePath = path.join(__dirname, 'src', folder);
-        const destinationPath = path.join(__dirname, repoName, folder);
-        console.log(sourcePath, "sourcePath");
-        console.log(destinationPath, "destinationPath");
-        if (fs.existsSync(sourcePath)) {
-            try {
-                fs.copyFileSync(sourcePath, destinationPath);
-                console.log(`Copied ${folder} to ${repoName}`);
-            } catch (err) {
-                console.error(`Error copying ${folder}:`, err);
-            }
-        } else {
-            console.error(`Folder ${folder} does not exist in the template`);
-        }
-    });
-} else {
-    console.log('Copying the entire template repository');
-    const sourcePath = path.join(__dirname); // Include '..' to go up one level from the current directory
-    const destinationPath = path.join(__dirname, repoName);
-
-    try {
-        fs.mkdirSync(destinationPath);
-        fs.copySync(sourcePath, destinationPath, { overwrite: true });
-        console.log(`Copied entire template repository to ${repoName}`);
-    } catch (err) {
-        console.error('Error copying the template repository:', err);
-    }
 }
 
 console.log(`Congrats! Happy hacking`);
