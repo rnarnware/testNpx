77e10a8a0cd562496d42139e1bfc7a7b6f8fab86
Rohit Narnaware <rnarnaware@cogitate.us>
2023-11-14 16:58:32 +0530

    updated new code

--- bin/cli.js
+++ bin/cli.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 const { execSync } = require('child_process');
-const { inherits } = require('util');
+const fs = require('fs-extra');
+const path = require('path');
 
 const runCommand = command => {
     try {
         execSync(`${command}`, { stdio: 'inherit'});
-    }catch(e){
+    } catch (e) {
         console.log('failed to exec ', e);
         return false;
     }
@@ -13,15 +14,33 @@ const runCommand = command => {
 }
 
 const repoName = process.argv[2];
-const giCheckoutCommand = `git clone --depth 1 https://github.com/rnarnware/testNpx ${repoName}`
+const folderName = process.argv[3];
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/rnarnware/testNpx ${repoName}`;
 const installDepsCommand = `cd ${repoName} && npm install`;
 
-console.log(`cloning the repo with name ${repoName}`);
-const checkOut = runCommand(giCheckoutCommand);
+console.log(`Cloning the repo with name ${repoName}`);
+const checkOut = runCommand(gitCheckoutCommand);
 
-if(!checkOut) process.exit(-1);
+if (!checkOut) process.exit(-1);
 
-console.log('insallling dependnecies for ', repoName);
-const installedlDeps = runCommand(installDepsCommand)
-if(!installedlDeps) process.exit(-1);
-console.log(`congrats happy hacking`);
+console.log(`Installing dependencies for ${repoName}`);
+const installedDeps = runCommand(installDepsCommand);
+
+if (!installedDeps) process.exit(-1);
+
+if (folderName) {
+    console.log(`Copying specific folder: ${folderName}`);
+    const sourcePath = path.join(__dirname, repoName, folderName);
+    const destinationPath = path.join(process.cwd(), folderName);
+
+    try {
+        fs.copySync(sourcePath, destinationPath, { overwrite: true });
+        console.log(`Copied ${folderName} to ${destinationPath}`);
+        console.log(`Congrats! Happy hacking in ${folderName}`);
+    } catch (err) {
+        console.error('Error copying the folder:', err);
+        process.exit(-1);
+    }
+} else {
+    console.log(`Congrats! Happy hacking in ${repoName}`);
+}
