18ffe41ca17ead5ff64b62f0c3f5a4e2c9f74324
Rohit Narnaware <rnarnaware@cogitate.us>
2023-11-14 16:36:19 +0530

    fixed path

--- bin/cli.js
+++ bin/cli.js
@@ -3,9 +3,9 @@ const { execSync } = require('child_process');
 const fs = require('fs-extra');
 const path = require('path');
 
-const runCommand = (command, cwd) => {
+const runCommand = (command) => {
     try {
-        execSync(`${command}`, { stdio: 'inherit', cwd });
+        execSync(command, { stdio: 'inherit' });
     } catch (e) {
         console.log('failed to exec ', e);
         return false;
@@ -25,39 +25,34 @@ const gitRepoURL = 'https://github.com/rnarnware/testNpx';
 const repoPath = path.join(__dirname, repoName);
 const folderPath = path.join(repoPath, folderName);
 const gitCheckoutCommand = `git clone --depth 1 --filter=blob:none --no-checkout ${gitRepoURL} ${repoName}`;
-const gitSparseCheckoutCommand = `cd ${repoName} && git sparse-checkout set ${folderName}`;
-const installDepsCommand = `cd ${repoPath} && npm install`;
-// const installFolderDepsCommand = `cd ${folderPath} && npm install`;
+const gitSparseCheckoutCommand = `git -C ${repoPath} sparse-checkout set ${folderName}`;
+const installDepsCommand = `npm install --prefix ${repoPath}`;
 
 console.log(`Cloning the repo with name ${repoName}`);
-const checkout = runCommand(gitCheckoutCommand, __dirname);
+const checkout = runCommand(gitCheckoutCommand);
 
 if (!checkout) {
     console.log('Failed to clone the repository');
     process.exit(-1);
 }
 
+console.log('Creating the folder structure');
+fs.ensureDirSync(repoPath);
+
 console.log('Installing all dependencies for ', repoName);
-const installedDeps = runCommand(installDepsCommand, __dirname);
+const installedDeps = runCommand(installDepsCommand);
+
 if (!installedDeps) {
     console.log('Failed to install dependencies');
     process.exit(-1);
 }
 
 console.log(`Checking out the specific folder: ${folderName}`);
-const sparseCheckout = runCommand(gitSparseCheckoutCommand, __dirname);
+const sparseCheckout = runCommand(gitSparseCheckoutCommand);
 
 if (!sparseCheckout) {
     console.log(`Failed to checkout the specific folder: ${folderName}`);
     process.exit(-1);
 }
-
-console.log(`Installing dependencies for the specified folder: ${folderName}`);
-// const installedFolderDeps = runCommand(installFolderDepsCommand, __dirname);
-
-// if (!installedFolderDeps) {
-//     console.log(`Failed to install dependencies for ${folderName}`);
-//     process.exit(-1);
-// }
 
 console.log(`Congrats! Happy hacking`);
