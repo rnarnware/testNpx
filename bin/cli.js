e5469084c3ddc37979d8de147cb611979f5af53e
Rohit Narnaware <rnarnaware@cogitate.us>
2023-11-14 20:12:14 +0530

    tested

--- bin/cli.js
+++ bin/cli.js
@@ -14,7 +14,7 @@ const runCommand = command => {
 }
 
 const repoName = process.argv[2];
-const folderName = process.argv[3];
+const folderNames = process.argv[3].split(',').map(folder => folder.trim()); // Split folder names by comma
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/rnarnware/testNpx ${repoName}`;
 const installDepsCommand = `cd ${repoName} && npm install`;
 
@@ -28,24 +28,24 @@ const installedDeps = runCommand(installDepsCommand);
 
 if (!installedDeps) process.exit(-1);
 
-if (folderName) {
-    console.log(`Copying specific folder: ${folderName}`);
-    const sourcePath = path.join(process.cwd(), repoName, 'src', folderName);
-    const destinationPath = path.join(process.cwd(), folderName);
-    console.log(sourcePath, "sourcePath");
+if (folderNames.length > 0) {
+    console.log(`Copying specific folders: ${folderNames.join(', ')}`);
+    
+    folderNames.forEach(folderName => {
+        const sourcePath = path.join(process.cwd(), repoName, 'src', folderName);
+        const destinationPath = path.join(process.cwd(), folderName);
 
-    try {
-        fs.copySync(sourcePath, destinationPath, { overwrite: true });
-        console.log(`Copied ${folderName} to ${destinationPath}`);
-        console.log(`Congrats! Happy hacking in ${folderName}`);
-    } catch (err) {
-        console.error('Error copying the folder:', err);
-        process.exit(-1);
-    }finally {
-        // Delete the cloned repository after copying the folder
+        try {
+            fs.copySync(sourcePath, destinationPath, { overwrite: true });
+            console.log(`Copied ${folderName} to ${destinationPath}`);
+        } catch (err) {
+            console.error(`Error copying the folder ${folderName}:`, err);
+        } // Delete the cloned repository after copying the folder
         fs.removeSync(repoName);
-        console.log(`Deleted the cloned repository: ${repoName}`);
-    }
+        // console.log(`Deleted the cloned repository: ${repoName}`);
+    });
+
+    console.log(`successfully cloned ${folderNames.join(', ')}`);
 } else {
     console.log(`Congrats! Happy hacking in ${repoName}`);
 }
