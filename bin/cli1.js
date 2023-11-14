523eb892bb2ca422d3a15f6e3ffd273848c8e7b5
Rohit Narnaware <rnarnaware@cogitate.us>
2023-11-14 19:19:16 +0530

    "version": "0.0.2",

---
+++ bin/cli1.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+const { execSync } = require('child_process');
+const fs = require('fs-extra');
+const path = require('path');
+
+const runCommand = command => {
+    try {
+        execSync(`${command}`, { stdio: 'inherit'});
+    } catch (e) {
+        console.log('failed to exec ', e);
+        return false;
+    }
+    return true;
+}
+
+const repoName = process.argv[2];
+const folderName = process.argv[3];
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/rnarnware/testNpx ${repoName}`;
+const installDepsCommand = `cd ${repoName} && npm install`;
+
+console.log(`Cloning the repo with name ${repoName}`);
+const checkOut = runCommand(gitCheckoutCommand);
+
+if (!checkOut) process.exit(-1);
+
+console.log(`Installing dependencies for ${repoName}`);
+const installedDeps = runCommand(installDepsCommand);
+
+if (!installedDeps) process.exit(-1);
+
+if (folderName) {
+    console.log(`Copying specific folder: ${folderName}`);
+    const sourcePath = path.join(process.cwd(), repoName, 'src', folderName);
+    const destinationPath = path.join(process.cwd(), folderName);
+    console.log(sourcePath, "sourcePath");
+
+    try {
+        fs.copySync(sourcePath, destinationPath, { overwrite: true });
+        console.log(`Copied ${folderName} to ${destinationPath}`);
+        console.log(`Congrats! Happy hacking in ${folderName}`);
+    } catch (err) {
+        console.error('Error copying the folder:', err);
+        process.exit(-1);
+    }
+} else {
+    console.log(`Congrats! Happy hacking in ${repoName}`);
+}
